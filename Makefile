1b1f505064fdffb8e1b7b62eef35ef23450b4b2d
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-05-11 21:07:02 +0200

    split into libsi_units and test_si_units

--- Makefile
+++ Makefile
@@ -2,10 +2,16 @@ MMC=mmc
 MCFLAGS=--use-grade-subdirs
 MLLIBS=--ml generic_math
 
+SI_UNIT_SUBS := $(wildcard si_units.*.m)
+
 .PHONY: test
-test: si_units
+test: test_si_units
 	@./$<
 
-si_units: si_units.m
+.PHONY: libsi_units
+libsi_units: si_units.m $(SI_UNITS_SUBS)
+	$(MMC) $(MCFLAGS) -m $@ $(MLLIBS)
+
+test_si_units: test_si_units.m
 	$(MMC) $(MCFLAGS) -m $@ $(MLLIBS)
 
--- si_units.m
+++ si_units.m
@@ -133,8 +133,6 @@
 
 :- func exp_to_doc(exp) = doc.
 
-:- pred main(io::di, io::uo) is det.
-
 %------------------------------------------------------------------------------%
 %------------------------------------------------------------------------------%
 
@@ -382,40 +380,6 @@ digit_to_sub('8') = '₈'.
 digit_to_sub('9') = '₉'.
 digit_to_sub('+') = '₊'.
 digit_to_sub('-') = '₋'.
-
-%------------------------------------------------------------------------------%
-% Test code for area, goal is to have compile time type checking
-%------------------------------------------------------------------------------%
-
-:- type rect
-    ---> rect(dimmed_value, dimmed_value).
-:- inst rect
-    ---> rect(metre, metre).
-
-:- func area(rect) = dimmed_value.
-%:- func area(rect::in(rect)) = (dimmed_value::out(dimmed_value)) is det.
-
-area(rect(A, B)) = A * B.
-
-main(!IO) :-
-    print_test("Unit of length", metre, !IO),
-    print_test("Area rect(2m, 3m)", area(rect(2.0*m, 3.0*m)), !IO),
-    print_test("Velocity", m/s, !IO),
-    print_test("Acceleration", m/(s**2), !IO),
-    print_test("Hertz", 1.0/s, !IO),
-    print_test("nano metres", m `exp` -9, !IO),
-    print_test("millimetres", 1.0e-3*m, !IO),
-    print_test("cube metres", m*m*m, !IO),
-    print_test("AU", 'AU', !IO),
-    print_test("lightyear", ly, !IO).
-
-:- pred print_test(string::in, T::in, io::di, io::uo) is det.
-
-print_test(Name, Entity, !IO) :-
-    io.print(Name, !IO),
-    io.print(" = ", !IO),
-    write_doc(format(Entity), !IO),
-    io.nl(!IO).
 
 %------------------------------------------------------------------------------%
 % Initialisation code for pretty printing
