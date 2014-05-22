366f64410faf564a7aeed1e217744a0dbda0825b
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-05-22 22:55:48 +0200

    testing norm func

--- test_si_units.m
+++ test_si_units.m
@@ -56,6 +56,8 @@ main(!IO) :-
     print_test("4 m + 6 m", 4.0*m + m(6.0), !IO),
     print_test("30 s - 2 s", 30.0*s - 2.0*s, !IO),
     print_test("[°C]", kelvin - 273.15, !IO),
+    print_test("norm(m*m*m)", norm((m*m*m)^dim), !IO),
+    print_test("norm(m**3)", norm((m**3)^dim), !IO),
     print_test("m² × m³", (m*m) * (m*m*m), !IO),
     print_test("Area rect(2m, 3m)", area(rect(m(2.0), m(3.0))), !IO),
     print_test("Velocity", m/s, !IO),
